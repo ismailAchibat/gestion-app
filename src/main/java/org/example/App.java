7ca2dd3733dbcb8828f1a295145c6e4d31d47a80
hend zormati <hend.zormati@esprit.tn>
2026-01-23 10:06:40 +0100

    changed scanbase

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "org.example")
+@SpringBootApplication(scanBasePackages = "fr.cours.info.tp.ferroviaire")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
