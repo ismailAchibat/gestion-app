fde8201e8bffb5de7619052ebec70bf24bb25bd5
e2404931 <messomba700@gmail.com>
2026-01-23 11:58:26 +0100

    Travail 25 : Affichage train avec Thymeleaf

---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,8 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    // Spring Boot + Thymeleaf: auto-config suffit généralement.
+}
