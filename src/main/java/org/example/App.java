bb5bf7bf332cf6b28cbfe056b025dc17a323b8ab
e2404931 <messomba700@gmail.com>
2026-01-23 10:25:49 +0100

    application.properties

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "fr.cours.info.tp.ferroviaire")
+@SpringBootApplication(scanBasePackages = {"org.example","fr.cours.info.tp.ferroviaire"})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
