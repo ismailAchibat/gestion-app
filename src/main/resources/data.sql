e0ec1560a44149cbb360be54078c43c60a1bca47
ismailAchibat <ismailachibat4@gmail.com>
2026-02-01 13:01:57 +0100

    remplir la base de données

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
-insert into gare(id, nom, nombre_quais) values (0, 'Le Mans', 1)
-insert into train(id, matricule) values (0, '1234')
+insert into gare(id, nom, nombre_quais) values (0, 'Le Mans', 1);
+insert into gare(id, nom, nombre_quais) values (1, 'Paris Montparnasse', 28);
+insert into gare(id, nom, nombre_quais) values (2, 'Lille Flandres', 17);
+insert into gare(id, nom, nombre_quais) values (3, 'Marseille Saint-Charles', 16);
+insert into gare(id, nom, nombre_quais) values (4, 'Bordeaux Saint-Jean', 15);
+
+insert into train(id, matricule) values (0, '1234');
+insert into train(id, matricule) values (1, '5678');
+insert into train(id, matricule) values (2, 'ABCD');
+insert into train(id, matricule) values (3, 'EFGH');
+insert into train(id, matricule) values (4, 'IJKL');
